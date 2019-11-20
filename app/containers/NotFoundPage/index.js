8840719e26bef4c868cfce61fd25e5a3fbea6c4c
Daniel Belmonte <d.belmonte.amoros@accenture.com>
2019-11-20 17:43:36 +0100

    more changes

--- app/containers/NotFoundPage/index.js
+++ app/containers/NotFoundPage/index.js
@@ -8,12 +8,26 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
+import styled from 'styled-components';
+
 import messages from './messages';
+
+const Wrapper = styled.div`
+  text-align: center;
+`;
 
 export default function NotFound() {
   return (
-    <h1>
-      <FormattedMessage {...messages.header} />
-    </h1>
+    <Wrapper>
+      <h1>
+        <FormattedMessage {...messages.header} />
+      </h1>
+      <p>
+        <FormattedMessage {...messages.message} />
+      </p>
+      <button type="button">
+        <FormattedMessage {...messages.go_home} />
+      </button>
+    </Wrapper>
   );
 }
--- app/containers/NotFoundPage/messages.js
+++ app/containers/NotFoundPage/messages.js
@@ -6,10 +6,19 @@
 import { defineMessages } from 'react-intl';
 
 export const scope = 'app.containers.NotFoundPage';
+export const common = 'app.common';
 
 export default defineMessages({
   header: {
     id: `${scope}.header`,
-    defaultMessage: 'This is the NotFoundPage container!',
+    defaultMessage: 'Resource not found',
+  },
+  message: {
+    id: `${scope}.message`,
+    defaultMessage: 'Oops! You may try something different...',
+  },
+  go_home: {
+    id: `${common}.go_home`,
+    defaultMessage: 'Go Home',
   },
 });
