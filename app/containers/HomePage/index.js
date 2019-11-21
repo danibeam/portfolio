e3cecd94a1376fda53f8f07ca7f2cce7aba6469d
Daniel Belmonte <d.belmonte.amoros@accenture.com>
2019-11-21 17:23:42 +0100

    Griding Home page

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -7,25 +7,39 @@
 
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import { Container, Row, Col } from 'react-grid-system';
 
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import Footer from 'components/Footer';
 
 import messages from './messages';
 
-const Wrapper = styled.div`
-  width: 90%;
-  margin: 0 auto;
-`;
-
 export default function HomePage() {
   return (
     <React.Fragment>
-      <Wrapper>
+      <Container>
         <h1>
           <FormattedMessage {...messages.header} />
         </h1>
-      </Wrapper>
+        <Row>
+          <Col sm={6}>
+            <p>
+              Aliqua exercitation exercitation proident proident incididunt non
+              exercitation labore. Anim pariatur do aliquip proident excepteur
+              consequat pariatur. Qui ut est minim sunt consectetur sint dolore
+              do eiusmod labore laborum proident.
+            </p>
+          </Col>
+          <Col sm={6}>
+            <p>
+              Aliqua exercitation exercitation proident proident incididunt non
+              exercitation labore. Anim pariatur do aliquip proident excepteur
+              consequat pariatur. Qui ut est minim sunt consectetur sint dolore
+              do eiusmod labore laborum proident.
+            </p>
+          </Col>
+        </Row>
+      </Container>
       <Footer />
     </React.Fragment>
   );
